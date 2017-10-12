773f0391e15b29230e60ff4ae96c5127402446f1
Eugene <evgenykrutpro@gmail.com>
2017-10-12 21:22:47 +0300

    2 update task 3

--- Lesson_2/3/src/Main.java
+++ Lesson_2/3/src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Begin");
-        Assembly line = new Assembly(new CreatureOfBody(), new CreatureOfEngine(),new CreatureOfTower());
+
+        ILineStep Body = new CreatureOfBody();
+        ILineStep Engine = new CreatureOfEngine();
+        ILineStep Tower = new CreatureOfTower();
+
+        Assembly line = new Assembly((CreatureOfBody) Body,(CreatureOfEngine) Engine,(CreatureOfTower) Tower);
         Tank tank = (Tank) line.assembleProduct(new Tank());
     }
 }
