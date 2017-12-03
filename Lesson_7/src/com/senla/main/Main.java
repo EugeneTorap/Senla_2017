50386690ca7639457b69859286fdfdcd77466a1f
Eugene <evgenykrutpro@gmail.com>
2017-12-03 11:37:10 +0300

    log config

--- Lesson_7/src/com/senla/main/Main.java
+++ Lesson_7/src/com/senla/main/Main.java
@@ -2,9 +2,11 @@ package com.senla.main;
 
 import com.senla.view.facade.OnlineBookStore;
 import com.senla.ui.MenuController;
+import org.apache.log4j.PropertyConfigurator;
 
 public class Main {
     public static void main(String[] args) {
+        PropertyConfigurator.configure("resources/log4j.properties");
 
         MenuController menuController = new MenuController();
         menuController.run();
