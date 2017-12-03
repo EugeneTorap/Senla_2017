cfb45913c7782a007c18dde7db9aeca01bf767fa
Eugene <evgenykrutpro@gmail.com>
2017-12-03 20:54:15 +0300

    set changes

--- Lesson_7/src/com/senla/csv/CSVWorker.java
+++ Lesson_7/src/com/senla/csv/CSVWorker.java
@@ -1,9 +1,7 @@
 package com.senla.csv;
 
 import com.senla.annotations.CsvEntity;
-import com.senla.controller.repositories.*;
-import com.senla.model.entity.*;
-import com.senla.model.entity.Reader;
+import com.senla.model.entity.Entity;
 import com.senla.util.*;
 import org.apache.log4j.Logger;
 
@@ -22,7 +20,7 @@ public class CSVWorker {
         while (true) {
             try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, false))) {
                 for (Entity entity : entities) {
-                    bw.write(entity.toString() + "\n");
+                    bw.write(entity + "\n");
                 }
             } catch (IOException e) {
                 LOGGER.error(e.getMessage());
@@ -50,56 +48,7 @@ public class CSVWorker {
             }
             break;
         }
+        SettingChanges.setChanges(clazz, csvEntity, strings.toArray(new String[0]));
 
-        String[] lines = strings.toArray(new String[0]);
-        int index;
-        switch (clazz.getSimpleName()){
-            case "Book":
-                List<Book> books = Parser.parseBook(lines, csvEntity.valuesSeparator());
-                for (Book book : books){
-                    if ((index = ArrayWorker.searchIndex(books, book.getId())) != -1){
-                        books.set(index, book);
-                    } else {
-                        books.add(book);
-                    }
-                }
-                BookRepository.getInstance().setBooks(books);
-                break;
-            case "Order":
-                List<Order> orders = Parser.parseOrder(lines, BookRepository.getInstance().getBooks(),
-                        csvEntity.valuesSeparator());
-                for (Order order : orders){
-                    if ((index = ArrayWorker.searchIndex(orders, order.getId())) != -1){
-                        orders.set(index, order);
-                    } else {
-                        orders.add(order);
-                    }
-                }
-                OrderRepository.getInstance().setOrders(orders);
-                break;
-            case "Reader":
-                List<Reader> readers = Parser.parseReader(lines, csvEntity.valuesSeparator());
-                for (Reader reader : readers){
-                    if ((index = ArrayWorker.searchIndex(readers, reader.getId())) != -1){
-                        readers.set(index, reader);
-                    } else {
-                        readers.add(reader);
-                    }
-                }
-                ReaderRepository.getInstance().setReaders(readers);
-                break;
-            case "Request":
-                List<Request> requests = Parser.parseRequest(lines, BookRepository.getInstance().getBooks(),
-                        ReaderRepository.getInstance().getReaders(), csvEntity.valuesSeparator());
-                for (Request request : requests){
-                    if ((index = ArrayWorker.searchIndex(requests, request.getId())) != -1){
-                        requests.set(index, request);
-                    } else {
-                        requests.add(request);
-                    }
-                }
-                RequestRepository.getInstance().setRequests(requests);
-                break;
-        }
     }
 }
